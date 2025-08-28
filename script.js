981660b0fa6b4665eac2f6676d273b2de1167182
pratibha-rt <pratibha.sept19@gmail.com>
2025-08-28 19:28:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function chunkStr(str, size) {
     let chunks = [];
-    for(let i = 0; i < str.length; i = i + size) {
+    for (let i = 0; i < str.length; i = i + size) {
         let subStr = str.slice(i, i + size);
         chunks.push(subStr);
     }
-
     return chunks;
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+const size = parseInt(prompt("Enter Chunk Size."), 10);
 alert(chunkStr(str, size));
